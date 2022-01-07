a3194edb6ba32a461e055b3a856b719f6bdc1896
joan9611 <joan9611@gmail.com>
2022-01-07 10:02:33 +0100

    commit 6

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
+      { path: '/index', component: () => import('pages/Index.vue') },
       { path: '/about', component: () => import('pages/About.vue') }
     ]
   },
