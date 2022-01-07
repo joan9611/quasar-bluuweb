1893afe6367a49696b531129985611d01ff4006d
joan9611 <joan9611@gmail.com>
2022-01-07 09:43:56 +0100

    cuarto commit

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import { route } from 'quasar/wrappers'
 import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
 import routes from './routes'
 
-// Linea de prueba
+
+//
 
 
 
